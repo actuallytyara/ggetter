5f5c191ebfba65a0cfa6b13ce81e1fabe8aafd2d
actuallytyara <ghatax123@gmail.com>
2026-07-29 08:44:21 +0700

    Provider test

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,27 @@
 <?php
 
-return [
-    App\Providers\AppServiceProvider::class,
-];
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+    }
+
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+}
